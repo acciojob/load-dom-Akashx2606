7902f302bb5fe2cf77ca63c8d9f425cb60698d01
Akashx2606 <137022133+Akashx2606@users.noreply.github.com>
2025-06-08 15:32:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-//your JS code here. If required.
-
-document.addEventListener('DOMContentLoaded', function() {
-            // Write the string to the body
-            document.body.innerHTML = "DOM load success";
-        });
+document.addEventListener("DOMLoaded", function(){
+	document.body.innerHTML = "DOM load success";
+})
